454bd20c4ed818acf0bff77e0ae54a6fb3307408
wuchibiao <1923356658@qq.com>
2025-10-20 23:21:23 +0800

    fix: createWebHashHistory

--- src/renderer/src/router/index.js
+++ src/renderer/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '@renderer/views/layout/index.vue'
 import SceneCreate from '@renderer/views/scene/scene-create/index.vue'
 import SceneManage from '@renderer/views/scene/index.vue'
